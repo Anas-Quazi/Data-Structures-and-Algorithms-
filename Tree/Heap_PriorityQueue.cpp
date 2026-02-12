647e54a4197280671bdf4ec2345f3f4399644524
Anas Quazi <anasqazi869@gmail.com>
2026-02-12 23:09:27 +0530

    implementation of heap

---
+++ Tree/Heap_PriorityQueue.cpp
@@ -0,0 +1,59 @@
+//! heap data structure in C++
+
+#include <iostream>
+#include <vector>
+
+
+//? class for implementing heap (max heap)
+class heap {
+
+public:
+    std :: vector<int> arr;
+    int size;
+
+    //^ constructor for initialization
+    heap() {
+        arr.push_back(-1);
+        size = 0;
+    }
+
+    //^ method to insert value
+    void insert(int val) {
+
+        size += 1;
+        int idx = size;
+        arr[idx] = val;
+
+        //todo place value to its correct position
+        while(idx > 1) {
+            int par = idx / 2;
+
+            if(arr[par] < arr[idx]) {
+                std :: swap(arr[par], arr[idx]);
+                idx = par;
+            }
+            else {
+                break;;
+            }
+        }
+    }
+
+    void print() {
+        for(int i=1; i<=size; i++) {
+            std :: cout << arr[i] << " ";
+        }
+    }
+};
+
+int main() {
+
+    heap h;
+    h.insert(10);
+    h.insert(16);
+    h.insert(8);
+    h.insert(25);
+
+    h.print();
+
+    return 0;
+}
